02169725e8f80e58ab4dd222c9967b2bd2a35281
Anders Myrman <amyhrman@gmail.com>
2023-10-10 11:55:55 +0200

    Add AppUI class

---
+++ src/main/java/com/example/AppUI.java
@@ -0,0 +1,54 @@
+package com.example;
+
+import javax.swing.*;
+
+import java.awt.event.*;
+
+public class AppUI implements ActionListener {
+
+  public AppUI() {
+
+    JLabel label = new JLabel("Java Music Player");
+
+    JFrame.setDefaultLookAndFeelDecorated(true);
+    JFrame frame = new JFrame("Java Music Player");
+    frame.setSize(300, 150);
+    frame.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+
+    // Add JOptionPane to close
+    frame.addWindowListener(new java.awt.event.WindowAdapter() {
+
+      @Override
+      public void windowClosing(java.awt.event.WindowEvent windowEvent) {
+        int confirmed = JOptionPane.showConfirmDialog(null,
+            "Are you sure you want to exit the application?", "Exit Application Confirmation",
+            JOptionPane.YES_NO_OPTION);
+
+        if (confirmed == JOptionPane.YES_OPTION) {
+          System.exit(0);
+        }
+      }
+    });
+
+    JButton openButton = new JButton("Open");
+    openButton.setBounds(10, 10, 80, 25);
+
+    openButton.addActionListener(this);
+
+    frame.add(openButton);
+
+    frame.add(label);
+
+    frame.setVisible(true);
+
+  }
+
+  @Override
+  public void actionPerformed(ActionEvent e) {
+    JFileChooser fileChooser = new JFileChooser();
+    int returnValue = fileChooser.showOpenDialog(null);
+    if (returnValue == JFileChooser.APPROVE_OPTION) {
+      // Do something with the selected file
+    }
+  }
+}

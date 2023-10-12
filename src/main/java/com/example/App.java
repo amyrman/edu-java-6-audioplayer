34d979770d4c449d52628693cae5fd51c6603721
Anders Myrman <amyhrman@gmail.com>
2023-10-12 15:18:45 +0200

    Change code base to use JavaFX, add/fix functions

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,7 +1,17 @@
 package com.example;
 
-public class App {
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class App extends Application {
     public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        // Create and show the UI
         AppUI appUI = new AppUI();
+        appUI.show();
     }
 }
--- src/main/java/com/example/AppUI.java
+++ src/main/java/com/example/AppUI.java
@@ -1,224 +1,166 @@
 package com.example;
 
-import java.awt.BorderLayout;
-import java.awt.FlowLayout;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
-import javax.swing.DefaultListModel;
-import javax.swing.JButton;
-import javax.swing.JComboBox;
-import javax.swing.JFileChooser;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JList;
-import javax.swing.JOptionPane;
-import javax.swing.JPanel;
-import javax.swing.JScrollPane;
-import javax.swing.UIManager;
-import javax.swing.UnsupportedLookAndFeelException;
-import javax.swing.filechooser.FileNameExtensionFilter;
+import javafx.application.Platform;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.ButtonBar;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+public class AppUI {
+    private MediaPlayer mediaPlayer;
+    private ObservableList<String> listModel;
+    private ListView<String> fileList;
+    private ComboBox<String> sortBox;
 
-import javafx.scene.media.MediaException;
+    public void show() {
+        // Create the UI
+        Label label = new Label("Java Music Player");
 
-public class AppUI implements ActionListener {
-  private AudioPlayer audioPlayer;
-  private DefaultListModel<String> listModel;
-  private JList<String> fileList;
-  JScrollPane scrollPane = new JScrollPane(fileList);
-  private JComboBox<String> sortBox;
+        fileList = new ListView<>();
+        listModel = FXCollections.observableArrayList();
+        fileList.setItems(listModel);
 
-  public AppUI() {
-    audioPlayer = new AudioPlayer();
+        Button openButton = new Button("Open");
+        openButton.setOnAction(e -> openFileExplorer());
 
-    // Set the look and feel of the UI to system
-    try {
-      UIManager.setLookAndFeel(
-          UIManager.getSystemLookAndFeelClassName());
-    } catch (UnsupportedLookAndFeelException | ClassNotFoundException | InstantiationException
-        | IllegalAccessException e) {
-      e.printStackTrace();
+        Button playPauseButton = new Button("Play/Pause");
+        playPauseButton.setOnAction(e -> playPauseSelectedFile());
+        
+        Button stopButton = new Button("Stop");
+        stopButton.setOnAction(e -> stopSelectedFile());
+
+        sortBox = new ComboBox<>();
+        sortBox.getItems().addAll("Ascending", "Descending");
+        sortBox.setValue("Ascending");
+        sortBox.setOnAction(e -> sortList());
+
+        Button exitButton = new Button("Exit");
+        exitButton.setOnAction(e -> {
+            int confirmed = showConfirmationDialog("Are you sure you want to exit the application?");
+            if (confirmed == 1) {
+                Platform.exit();
+            }
+        });
+
+        HBox buttonBox = new HBox(openButton, playPauseButton,  stopButton, sortBox, exitButton);
+        buttonBox.setSpacing(10);
+
+        VBox root = new VBox(label, fileList, buttonBox);
+        root.setSpacing(10);
+        root.setPadding(new Insets(10));
+
+        // Create the scene and stage
+        Scene scene = new Scene(root, 600, 400);
+        Stage stage = new Stage();
+        stage.setScene(scene);
+        stage.setTitle("Java Exercise #6: Java Music Player");
+
+        // Add a window listener to the stage to handle closing events
+        stage.setOnCloseRequest(e -> {
+            e.consume();
+            int confirmed = showConfirmationDialog("Are you sure you want to exit the application?");
+            if (confirmed == 1) {
+                Platform.exit();
+            }
+        });
+
+        // Show the stage
+        stage.show();
     }
 
-    // Create the list model and list objects
-    listModel = new DefaultListModel<>();
-    fileList = new JList<>(listModel);
-
-    // Create the frame object
-    JFrame frame = new JFrame("Java Exercise #6: Java Music Player");
-
-    // Add a window listener to the frame to handle closing events
-    frame.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
-    frame.addWindowListener(new java.awt.event.WindowAdapter() {
-      @Override
-      public void windowClosing(java.awt.event.WindowEvent windowEvent) {
-        int confirmed = JOptionPane.showConfirmDialog(null,
-            "Are you sure you want to exit the application?", "Exit Application Confirmation",
-            JOptionPane.YES_NO_OPTION);
-
-        if (confirmed == JOptionPane.YES_OPTION) {
-          System.exit(0);
+    private void openFileExplorer() {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Open Audio Files");
+        fileChooser.getExtensionFilters().addAll(
+                new FileChooser.ExtensionFilter("Audio Files", "*.mp3", "*.wav", "*.aiff", "*.au", "*.snd", "*.mid", "*.rmi"),
+                new FileChooser.ExtensionFilter("All Files", "*.*"));
+        List<File> selectedFiles = fileChooser.showOpenMultipleDialog(null);
+        if (selectedFiles != null) {
+            addFilesToList(selectedFiles);
         }
-      }
-    });
-
-    // Create the label object
-    JLabel label = new JLabel("Java Music Player");
-
-    // Create the main content panel with BorderLayout
-    JPanel contentPanel = new JPanel(new BorderLayout());
-
-    // Create open button object
-    JButton openButton = new JButton("Open");
-    openButton.setActionCommand("Open Explorer");
-    openButton.addActionListener(this);
-
-    JButton playStopButton = new JButton("Play/Stop");
-    playStopButton.setActionCommand("Play");
-    playStopButton.addActionListener(this);
-
-    JButton sortButton = new JButton("Sort");
-    sortButton.setActionCommand("Sort");
-    sortButton.addActionListener(this);
-
-    sortBox = new JComboBox<>();
-    sortBox.addItem("Ascending");
-    sortBox.addItem("Descending");
-    sortBox.addActionListener(this);
-    
-    // Create a panel for the buttons with FlowLayout
-    JPanel buttonPanel = new JPanel(new FlowLayout());
-
-    // Add the label and file list to the top and center of the content panel
-    contentPanel.add(label, BorderLayout.NORTH);
-    contentPanel.add(fileList, BorderLayout.CENTER);
-
-    // Add the buttons to the button panel
-    buttonPanel.add(openButton);
-    buttonPanel.add(playStopButton);
-    buttonPanel.add(sortBox);
-
-    // Add the button panel to the bottom of the content panel
-    contentPanel.add(buttonPanel, BorderLayout.SOUTH);
-
-    // Add the content panel to the frame
-    frame.add(contentPanel);
-
-    // Set the size and visibility of the frame
-    frame.setSize(600, 400);
-    frame.setLocationRelativeTo(null); // center window
-    frame.setVisible(true);
-  }
-
-  private void openFileExplorer() {
-    JFileChooser fileChooser = new JFileChooser();
-    setFileChooserDirectory(fileChooser);
-    FileNameExtensionFilter filter = new FileNameExtensionFilter(
-        "Audio Files", "mp3", "wav", "aiff", "au", "snd", "mid", "rmi");
-    fileChooser.setFileFilter(filter);
-    fileChooser.setMultiSelectionEnabled(true);
-
-    int returnValue = fileChooser.showOpenDialog(null);
-    if (returnValue == JFileChooser.APPROVE_OPTION) {
-      File[] selectedFiles = fileChooser.getSelectedFiles();
-      addFilesToList(selectedFiles);
     }
-  }
 
-  private void playStopSelectedFile() {
-    int selectedIndex = fileList.getSelectedIndex();
-    if (selectedIndex != -1) {
-      String filePath = listModel.getElementAt(selectedIndex);
-      if (audioPlayer != null && audioPlayer.isPlaying()) {
-        // Stop the mp3 player if it's already playing
-        audioPlayer.stop();
-      } else {
-        // Start playing the mp3 file
-        try {
-          audioPlayer.play(filePath);
-        } catch (MediaException ex) {
-          ex.printStackTrace();
+    private void playPauseSelectedFile() {
+        int selectedIndex = fileList.getSelectionModel().getSelectedIndex();
+        if (selectedIndex != -1) {
+            String filePath = listModel.get(selectedIndex);
+            if (mediaPlayer != null && mediaPlayer.getStatus() == MediaPlayer.Status.PLAYING) {
+                // Pause the media player if it's already playing
+                mediaPlayer.pause();
+              } else if (mediaPlayer != null && mediaPlayer.getStatus() == MediaPlayer.Status.PAUSED) {
+                // Resume playback if the media player is paused
+                mediaPlayer.play();
+            } else {
+                // Start playing the media file
+                Media media = new Media(new File(filePath).toURI().toString());
+                mediaPlayer = new MediaPlayer(media);
+                mediaPlayer.play();
+            }
         }
-      }
     }
-  }
 
-  private void sortList() {
-    String selected = (String) sortBox.getSelectedItem();
-    List<String> list = new ArrayList<>();
-    for (int i = 0; i < listModel.getSize(); i++) {
-      list.add(listModel.get(i));
-    }
-    if (selected.equals("Ascending")) {
-      list.sort((s1, s2) -> s1.compareTo(s2));
-    } else if (selected.equals("Descending")) {
-      list.sort((s1, s2) -> s2.compareTo(s1));
-    }
-    listModel.clear();
-    for (String s : list) {
-      listModel.addElement(s);
-    }
-  }
-
-  @Override
-  public void actionPerformed(ActionEvent e) {
-    String actionCommand = e.getActionCommand();
-    switch (actionCommand) {
-      case "Open Explorer":
-        openFileExplorer();
-        break;
-      case "Play":
-        playStopSelectedFile();
-        break;
-      default:
-        // Handle the sortBox selection
-        if (e.getSource() == sortBox) {
-          sortList();
+    private void stopSelectedFile() {
+        if (mediaPlayer != null && mediaPlayer.getStatus() == MediaPlayer.Status.PLAYING) {
+            mediaPlayer.stop();
         }
-        break;
-      // Add cases for other actions as needed
     }
-  }
 
-  private void setFileChooserDirectory(JFileChooser fileChooser) {
-    File downloadsDir = new File(System.getProperty("user.home") + System.getProperty("file.separator") + "Downloads");
-    if (downloadsDir.exists() && downloadsDir.isDirectory()) {
-      fileChooser.setCurrentDirectory(downloadsDir);
-    } else {
-      fileChooser.setCurrentDirectory(new File(System.getProperty("user.home")));
-    }
-  }
-
-  private void addFilesToList(File[] files) {
-    for (File file : files) {
-      if (file.isFile()) {
-        try {
-          String filePath = file.getAbsolutePath();
-          listModel.addElement(filePath);
-        } catch (Exception ex) {
-          ex.printStackTrace();
+    private void sortList() {
+        String selected = sortBox.getSelectionModel().getSelectedItem();
+        List<String> list = new ArrayList<>(listModel);
+        if (selected.equals("Ascending")) {
+            list.sort(String::compareTo);
+        } else if (selected.equals("Descending")) {
+            list.sort((s1, s2) -> s2.compareTo(s1));
         }
-      }
+        listModel.setAll(list);
     }
-  }
+
+    private void addFilesToList(List<File> files) {
+        for (File file : files) {
+            if (file.isFile()) {
+                try {
+                    String filePath = file.getAbsolutePath();
+                    listModel.add(filePath);
+                } catch (Exception ex) {
+                    ex.printStackTrace();
+                }
+            }
+        }
+    }
+
+    private int showConfirmationDialog(String message) {
+        ButtonType yesButton = new ButtonType("Yes", ButtonBar.ButtonData.YES);
+        ButtonType noButton = new ButtonType("No", ButtonBar.ButtonData.NO);
+        Alert alert = new Alert(AlertType.CONFIRMATION, message, yesButton, noButton);
+        alert.setHeaderText(null);
+        alert.setTitle("Confirmation");
+        Optional<ButtonType> result = alert.showAndWait();
+        if (result.isPresent()) {
+            if (result.get() == yesButton) {
+                return 1;
+            } else if (result.get() == noButton) {
+                return 0;
+            }
+        }
+        return 0;
+    }
 }
-
-// // // Un-implemented buttons
-// // Create stop button object
-// JButton stopButton = new JButton("Stop");
-// stopButton.addActionListener(this);
-
-// // Create prev button object
-// JButton prevButton = new JButton("Prev");
-// prevButton.addActionListener(this);
-
-// // Create next button object
-// JButton nextButton = new JButton("Next");
-// nextButton.addActionListener(this);
-
-// buttonPanel.add(stopButton);
-// buttonPanel.add(prevButton);
-// buttonPanel.add(nextButton);
